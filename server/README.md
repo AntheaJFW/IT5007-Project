cfe234dc4e2fb6480d97e3221ac83f37383250db
AntheaJFW <wong.ji.fong@gmail.com>
2023-01-24 22:18:32 +0800

    Added express server with swagger and tested react build -> serve with express

---
+++ server/README.md
@@ -0,0 +1,23 @@
+# IT5007 Express server backend
+### Requirements
+- [yarn](https://yarnpkg.com/getting-started/install)
+
+### Setup
+```bash
+yarn install
+```
+
+### Commands
+Start development server
+```bash
+yarn start
+```
+
+Start production server
+```bash
+yarn serve
+```
+
+If port is not specified, starts app at `localhost:3001`.
+Once started, see [http://localhost:3001/api-docs](http://localhost:3001/api-docs) 
+for more information on apis available.
---
+++ server/routes/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *         '200':
+ *             description: Successfully return users from users API
+ *         '404':
+ *             description: User not found
+ *         '403':
+ *             description: Not enough permissions to find user
+ */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
