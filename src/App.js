c599bbc14faff7db8e97f7f58bbc90b3496aa1e8
Cara Chiang <carachiangeh@gmail.com>
2023-01-25 15:50:12 +0800

    Remove react starter files that are unused

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={
-                <ProtectedRoute>
-                    <p>ACCESSED!</p>
-                </ProtectedRoute>
-            } />
-            <Route path="/login" element={<Auth />} />
-            <Route path="*" element={<p>404 not found</p>} status={404}/>
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <p>ACCESSED!</p>
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/login' element={<Auth />} />
+        <Route path='*' element={<p>404 not found</p>} status={404} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
