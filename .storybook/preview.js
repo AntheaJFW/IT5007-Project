e63bcbd7199430712a5b84acbaeb526b53674db5
AntheaJFW <wong.ji.fong@gmail.com>
2023-02-06 21:56:27 +0800

    added toc nav and updated problem formulation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { DocsContainer } from '@storybook/addon-docs';
+import { BackToTop, TableOfContents } from 'storybook-docs-toc';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,5 +10,16 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    container: ({ children, ...rest }) => (
+      <React.Fragment>
+        <DocsContainer {...rest}>
+          <TableOfContents className='sbdocs sbdocs-toc--custom' />
+          {children}
+          <BackToTop className='sbdocs sbdocs-top--custom' />
+        </DocsContainer>
+      </React.Fragment>
+    ),
   },
 };
