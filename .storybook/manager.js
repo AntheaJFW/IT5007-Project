03695dc2c544f01ed61d32b1884aa8cb4e92f32e
AntheaJFW <wong.ji.fong@gmail.com>
2023-02-06 23:04:22 +0800

    revert changes on logo change as gh page served on subpath

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,14 +1,8 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
-import logo from './public/logo.png';
 
 const theme = create({
   base: 'light',
-
-  // Update this if the repo allows docs
-  brandUrl: 'https://antheajfw.github.io/IT5007-Project/',
-  brandImage: `/${logo}`,
-  brandTitle: 'IT5007 project',
 });
 
 addons.setConfig({
