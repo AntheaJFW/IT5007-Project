a09d48cb3710e5f362f0519016dd4032d9462181
AntheaJFW <wong.ji.fong@gmail.com>
2023-02-06 22:46:04 +0800

    updated baseurl

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,6 +5,8 @@ import logo from './public/logo.png';
 const theme = create({
   base: 'light',
 
+  // Update this if the repo allows docs
+  brandUrl: 'https://antheajfw.github.io/IT5007-Project/',
   brandImage: `/${logo}`,
   brandTitle: 'IT5007 project',
 });
